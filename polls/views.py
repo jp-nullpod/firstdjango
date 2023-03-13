ac89c0bc0b8d1aeb338be98023ea10f588ceeb1e
Jp Fortuno <jp.fortuno@nullpod.com>
2023-03-13 20:28:18 +0400

    polls views and templates added

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world, we are at the index")
+    questions = Question.objects.all()
+    context = {
+        'title': 'My awesome Questions list',
+        'questions': questions
+    }
+    return render(request, 'polls/index.html', context)
 
 def jp(request):
     return HttpResponse("Hello jp, we are at the index")
